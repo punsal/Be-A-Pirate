496d913892d0e8e99002de848fd2a97cb0297649
Mustafa Pamir Ünsal <p_unsal@msn.com>
2019-02-01 02:32:17 +0300

    Assets Imported & UI Created
    * Simple UI Placeholders
    * Simple Card Placeholder
    * Scale Controller is written and tested
    * Postprocessor imported
    * MetaSheets imported

---
+++ Be A Pirate/Assets/Scripts/Card Controllers/ScaleController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScaleController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.touchCount > 0)
+        {
+            transform.localScale = new Vector3(1.1f, 1.1f, 0.1f);
+        }
+        else
+        {
+            transform.localScale = new Vector3(1f, 1f, 0.1f);
+        }
+    }
+}
